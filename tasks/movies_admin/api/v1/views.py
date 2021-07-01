7cb96ac479dcac51d36b7b99cc47a5c49fe63d7d
Alexandr Trubetskov <a.trubetskov@corp.mail.ru>
2021-07-02 01:07:21 +0300

    Added docker-compose, postgres, nginx

--- tasks/movies_admin/api/v1/views.py
+++ tasks/movies_admin/api/v1/views.py
@@ -4,9 +4,19 @@ from django.views.generic.detail import BaseDetailView
 from django.http import JsonResponse
 from django.views.generic.list import BaseListView
 from django.core.paginator import Paginator
-from itertools import chain
 
-from movies.models import Filmwork, PersonRole, Person
+from movies.models import Filmwork
+
+from django.core.serializers.json import DjangoJSONEncoder
+from decimal import Decimal
+
+
+class MyDjangoJSONEncoder(DjangoJSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, Decimal):
+            return float(o)
+        return super().default(o)
 
 
 class MoviesListApi(BaseListView):
@@ -20,7 +30,6 @@ class MoviesListApi(BaseListView):
     def get_queryset(self, request):
         return self.model.objects.order_by('id').values('id', 'title', 'description', 'creation_date', 'rating', 'type').annotate(genres=ArrayAgg('genres__name', distinct=True)).annotate(directors=ArrayAgg('film_relation__person__name', filter=Q(film_relation__role='Director'), distinct=True)).annotate(writers=ArrayAgg('film_relation__person__name', filter=Q(film_relation__role='Writer'),distinct=True)).annotate(actors=ArrayAgg('film_relation__person__name', filter=Q(film_relation__role='Actor'), distinct=True)).values('id', 'title', 'description', 'creation_date', 'rating', 'type', 'genres', 'actors', 'directors', 'writers')
 
-
     def get_context_data(self, request, *, object_list=None, **kwargs):
 
         paginator = Paginator(self.get_queryset(request), 50)
@@ -29,15 +38,15 @@ class MoviesListApi(BaseListView):
 
         context = {
             'count': page_obj.paginator.count,
-            'total_pages': page_obj.paginator.num_pages,
-            'prev': page_obj.previous_page_number() if page_number.isdigit() and int(page_number) > 1 else (page_obj.paginator.num_pages if page_number == 'last' else None),
-            'next': page_obj.next_page_number(),
+            'total_pages': page_obj.paginator.num_pages - 1,
+            'prev': page_obj.previous_page_number() if page_number.isdigit() and int(page_number) > 1 else (page_obj.paginator.num_pages - 2 if page_number == 'last' else None),
+            'next':  None if page_number == 'last' or page_number == page_obj.paginator.num_pages - 1 else page_obj.next_page_number(),
             'results': list(page_obj),
         }
         return context
 
     def render_to_response(self, context, **response_kwargs):
-        return JsonResponse(context)
+        return JsonResponse(context, encoder=MyDjangoJSONEncoder)
 
 
 class MoviesDetailApi(BaseDetailView):
@@ -52,15 +61,12 @@ class MoviesDetailApi(BaseDetailView):
         pk = self.kwargs.get(self.pk_url_kwarg, None)
         return self.model.objects.filter(id=pk).values('id', 'title', 'description', 'creation_date', 'rating', 'type').annotate(genres=ArrayAgg('genres__name', distinct=True)).annotate(directors=ArrayAgg('film_relation__person__name', filter=Q(film_relation__role='Director'), distinct=True)).annotate(writers=ArrayAgg('film_relation__person__name', filter=Q(film_relation__role='Writer'),distinct=True)).annotate(actors=ArrayAgg('film_relation__person__name', filter=Q(film_relation__role='Actor'), distinct=True)).values('id', 'title', 'description', 'creation_date', 'rating', 'type', 'genres', 'actors', 'directors', 'writers')
 
-
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = {
-            'results': list(self.get_queryset()),
-        }
+        context = self.get_queryset()[0]
         return context
 
     def render_to_response(self, context, **response_kwargs):
-        return JsonResponse(context, safe=False)
+        return JsonResponse(context, encoder=MyDjangoJSONEncoder, safe=False)
 
 
 
--- tasks/movies_admin/manage.py
+++ tasks/movies_admin/manage.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3.7
 import os
 import sys
 
